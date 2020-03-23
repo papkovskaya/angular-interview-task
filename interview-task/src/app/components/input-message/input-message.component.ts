29c61f063ba6b443418fa313fca67bdbee9628a1
Volha Papkouskaya <Volha.Papkouskaya@thomsonreuters.com>
2020-03-23 16:59:10 +0300

    change name for input to input-message component

---
+++ interview-task/src/app/components/input-message/input-message.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-input-message',
+  templateUrl: './input-message.component.html',
+  styleUrls: ['./input-message.component.scss']
+})
+export class InputMessageComponent implements OnInit {
+
+  constructor() { }
+
+  public message: string = "";
+
+  ngOnInit(): void {
+  }
+
+}
